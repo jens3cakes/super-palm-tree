41a8898259c7708e542c388605722a4ed71e0664
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2019-08-17 15:29:14 -1000

    Username and tweet write to file

--- twitter.py
+++ twitter.py
@@ -4,34 +4,31 @@ import requests
 from bs4 import BeautifulSoup
 
 url = "https://twitter.com/search"
+subj_to_search = input("What would you like to find")
 
 results = requests.get(url,
-params={'q':'Devleague'}, 
- 
+params={'q':subj_to_search},
 headers={
      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel   Mac OS X 10_13_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.     3809.100 Safari/537.36'
  }
 )
 
 soup = BeautifulSoup(results.text, 'html.parser')
+recipe_for_soup = open("twitter_soup.txt", "a")
 
-#print(soup.div.prettify())
+def search_tweets(param):
+    usernames = soup.select("div.content strong.fullname")
+    for username in usernames:
+        tweet_searcher()
+        recipe_for_soup.write("\nThis is the username:" + username.text + "\n This is the tweet left:" + tweet_searcher())
 
-usernames = soup.select("div.tweet strong.fullname")
-
-tweets = soup.select("div.js-tweet-text-container")
-
-text_file = open("dleague.txt", "w")
-
-for username in usernames:
-    print(results)    
-    print(username.text)
-    text_file.write("\nUser: " + str(username.text))
-
+def tweet_searcher():
+    tweets = soup.select("div.content p.tweet-text")
     for tweet in tweets:
-        print(tweet.text)
-        text_file.write("\nComment: " + str(tweet.text))
-        
+        return(tweet.get_text())       
 
-
+if __name__ == "__main__":
+    search_tweets(subj_to_search)
+    tweet_searcher()
+    
 
