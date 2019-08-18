5572fd4b3030cef6769ffe14447a68cf5375ddcf
Jennifer Hikichi <jenniferhikichi@Jennifers-MacBook-Pro.local>
2019-08-17 17:24:32 -1000

    image searcher added

--- twitter.py
+++ twitter.py
@@ -20,15 +20,26 @@ def search_tweets(param):
     usernames = soup.select("div.content strong.fullname")
     for username in usernames:
         tweet_searcher()
-        recipe_for_soup.write("\nThis is the username:" + username.text + "\n This is the tweet left:" + tweet_searcher())
+        img_searcher()
+        recipe_for_soup.write("\nThis is the username: " + username.text + "\nThis is the tweet left: " + tweet_searcher() + "\nThis is the pic: " + img_searcher())
+    #recipe_for_soup.close()
 
 def tweet_searcher():
     tweets = soup.select("div.content p.tweet-text")
     for tweet in tweets:
-        return(tweet.get_text())       
+        return(tweet.get_text()) 
+
+def img_searcher():
+    images = soup.select("div.content div.AdaptiveMedia-photoContainer img")
+    for image in images:
+        return(image['src'])
+    else:
+        return("No image found")
 
 if __name__ == "__main__":
     search_tweets(subj_to_search)
     tweet_searcher()
+    img_searcher()
     
+
 
